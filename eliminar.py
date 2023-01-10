b843ba7175ad544edc673890456634d285c8e480
Juan Carlos Tibocha Espinosa <jctcec@gmail.com>
2023-01-10 19:17:46 +0100

    Correción errores 10-01-23

--- eliminar.py
+++ eliminar.py
@@ -1,58 +1,33 @@
 from tkinter import *
-from main import ventanaInicio
-from declarative_base import Session, engine, Base
 from sqlalchemy import delete
 
 
-class Eliminar():    
 
-    def eliminadoDatos(self, nombre):
-        
-        session = Session()
-
-        session.query(Producto).filter(Producto.name == nombre).delete()
-
-        session.commit()
-
-        session.close()
+def eliminadoDatos(nombre):
     
+    session = Session()
+    session.query(Producto).filter(Producto.name == nombre).delete()
+    session.commit()
+    session.close()
 
+
+def ventanaEliminar():
+    global ventanaEliminado
+    ventanaEliminado = Toplevel()
+    ventanaEliminado.title("Eliminado")
+    ventanaEliminado.state('zoomed')
     
-    def ventanaEliminar(self):
-
-        global ventanaEliminado
-        ventanaEliminado = Toplevel(ventanaInicio)
-        ventanaEliminado.title("Eliminado")
-        ventanaEliminado.state('zoomed')
-        
-        global imagen 
-        imagen = PhotoImage(file="imagen.png")
-        imagen = imagen.subsample(2,2)
-        Label(ventanaEliminado, image = imagen).pack()
-
-        Label(ventanaEliminado, text="Por favor el nombre del producto que desee eliminar:", bg="navy", fg="white", width="300", height="3", font=("Calibri", 15)).pack()
-        Label(ventanaEliminado, text="").pack()
-
-        global nombre
-
-
-        nombre = StringVar()
-
-
-        global eliminadoNombre
-
-
-
-        Label(ventanaEliminado, text="nombre").pack()
-        eliminadoNombre = Entry(ventanaEliminado, textvariable = nombre).pack()
-        
-        
-        eliminadoNombre = Entry(ventanaEliminado, textvariable = nombre)
-        eliminadoNombre.pack()
-        
-        
-
-        Label(ventanaEliminado, text= "").pack()
-        Button(ventanaEliminado, text="Continuar",height="3", width="30" , command = lambda: self.eliminadoDatos(nombre.get()) ).pack()
-
-        
+    global imagen 
+    imagen = PhotoImage(file="imagen.png")
+    imagen = imagen.subsample(2,2)
+    Label(ventanaEliminado, image = imagen).pack()
+    Label(ventanaEliminado, text="Por favor el nombre del producto que desee eliminar:", bg="navy", fg="white", width="300", height="3", font=("Calibri", 15)).pack()
+    Label(ventanaEliminado, text="").pack()
+    global nombre
+    nombre = StringVar()
+    global eliminadoNombre
+    Label(ventanaEliminado, text="Introduzca el nombre del producto: ").pack()
+    eliminadoNombre = Entry(ventanaEliminado, textvariable = nombre).pack()
+    
+    Label(ventanaEliminado, text= "").pack()
+    Button(ventanaEliminado, text="Continuar",height="3", width="30" , command = lambda: eliminadoDatos(nombre.get()) ).pack()
